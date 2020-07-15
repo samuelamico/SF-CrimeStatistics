6ab30e1114e9381efd323fbaa5c97e93a47b9d02
Samuel Amico <47945745+samuelamico@users.noreply.github.com>
2020-07-15 09:09:30 -0300

    Add files via upload

---
+++ startup.sh
@@ -0,0 +1,7 @@
+echo 'schema.registry.url=http://localhost:8081' >> /etc/kafka/connect-distributed.properties
+systemctl start confluent-zookeeper > startup/startup.log 2>&1
+systemctl start confluent-kafka > startup/startup.log 2>&1
+systemctl start confluent-schema-registry > startup/startup.log 2>&1
+systemctl start confluent-kafka-rest > startup/startup.log 2>&1
+systemctl start confluent-kafka-connect > startup/startup.log 2>&1
+systemctl start confluent-ksql > startup/startup.log 2>&1
